1de1e9b88a882249795f6112cba549758d65f8f7
Ali Hussain <alhu0002@stud.kea.dk>
2025-01-08 19:49:33 +0100

    1.første commit

---
+++ src/main/java/com/example/eksamentekd/EksamentekdApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamentekd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamentekdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamentekdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamentekd/EksamentekdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamentekd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamentekdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
